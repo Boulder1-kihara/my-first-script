0df80f5871a71d1e45e43384264a72d39ce25001
The Boulder <abelkihara8@gmail.com>
2024-12-15 21:24:01 +0300

    message

---
+++ sum.js
@@ -0,0 +1,7 @@
+function findsum(num1, num2){
+    return num1 + num2;
+}
+let number1 = 10;
+let number2 = 5;
+let sum = findsum(number1, number2);
+console.log("The sum of", number1, "and", number2, "is:", sum);
